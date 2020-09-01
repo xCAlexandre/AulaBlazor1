a1d8644245abeb0a172b73aa88adaf6aa40f75f0
Carlos <carlosalexandre@univem.edu.br>
2020-09-01 16:39:33 -0300

    teste

--- Shared/AFazer.cs
+++ Shared/AFazer.cs
@@ -2,19 +2,12 @@ using System;
 namespace aulablazor1.Shared
 {
     public class AFazer
-    {
+    {   
+        public int posicao{get ; set; }
         public string titulo { get; set; }
 
         public bool feito { get; set; }
-        public DateTime data;
-
-        public DateTime GetData(){
-            return data;
-        }
-
-        public void SetData(){
-            data = DateTime.Now;
-        }
+        public DateTime data{get ; set; }
 
     }
 }
